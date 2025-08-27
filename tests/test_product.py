48ae30095a83633aeed28ba10f1ef54b51b52d1d
alimad jafarov <kdafik@yandex.ru>
2025-08-28 01:58:01 +0300

    Написание тестов

---
+++ tests/test_product.py
@@ -0,0 +1,15 @@
+import pytest
+
+from src.main import Product
+
+
+@pytest.fixture
+def product_iphone():
+    return Product("Iphone 11", "Iphone 11", 1000.0, 5)
+
+
+def test_init(product_iphone):
+    assert product_iphone.name == "Iphone 11"
+    assert product_iphone.description == "Iphone 11"
+    assert product_iphone.price == 1000.0
+    assert product_iphone.quantity == 5
